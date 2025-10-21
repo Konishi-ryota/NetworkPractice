a89ec5d2c19c652d21ac5ebf05d34c4b9f2703a3
ryotakonishi <ryota.konishi@icloud.com>
2025-10-21 21:12:24 +0900

    [add]コミット終わり

---
+++ Assets/Scripts/GameLauncher.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion.Sockets;
+using Fusion;
+public class GameLauncher : MonoBehaviour, INetworkRunnerCallbacks
+{
+    [SerializeField]
+    private NetworkRunner networkRunnerPrefab;
+    [SerializeField]
+    private NetworkPrefabRef playerAvatarPrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private async void Start()
+    {
+        // NetworkRunnerを生成する
+        var networkRunner = Instantiate(networkRunnerPrefab);
+        // GameLauncherを、NetworkRunnerのコールバック対象に追加する
+        networkRunner.AddCallbacks(this);
+        var result = await networkRunner.StartGame(new StartGameArgs
+        {
+            GameMode = GameMode.Shared,
+        });
+        Debug.Log(result);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void INetworkRunnerCallbacks.OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    void INetworkRunnerCallbacks.OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+
+    void INetworkRunnerCallbacks.OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    // プレイヤーがセッションへ参加した時に呼ばれるコールバック
+    {
+        if (player == runner.LocalPlayer)
+        // セッションへ参加したプレイヤーが自分自身かどうかを判定する
+        {
+            var rand = UnityEngine.Random.insideUnitCircle * 5;
+            var spawnPosition = new Vector3(rand.x, 2f, rand.y);
+
+            runner.Spawn(playerAvatarPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+
+    void INetworkRunnerCallbacks.OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
+    void INetworkRunnerCallbacks.OnInput(NetworkRunner runner, NetworkInput input) { }
+    void INetworkRunnerCallbacks.OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+    void INetworkRunnerCallbacks.OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+    void INetworkRunnerCallbacks.OnConnectedToServer(NetworkRunner runner) { }
+    void INetworkRunnerCallbacks.OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
+    void INetworkRunnerCallbacks.OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+    void INetworkRunnerCallbacks.OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+    void INetworkRunnerCallbacks.OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+    void INetworkRunnerCallbacks.OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+    void INetworkRunnerCallbacks.OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    void INetworkRunnerCallbacks.OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+    void INetworkRunnerCallbacks.OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) { }
+    void INetworkRunnerCallbacks.OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
+    void INetworkRunnerCallbacks.OnSceneLoadDone(NetworkRunner runner) { }
+    void INetworkRunnerCallbacks.OnSceneLoadStart(NetworkRunner runner) { }
+}
