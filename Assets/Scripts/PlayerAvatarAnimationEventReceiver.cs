cc2e35b916df9e305d45e947e9ce244686cc14af
ryotakonishi <ryota.konishi@icloud.com>
2025-10-23 00:24:44 +0900

    [add]アバターのアニメーション用スクリプト追加

---
+++ Assets/Scripts/PlayerAvatarAnimationEventReceiver.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerAvatarAnimationEventReceiver : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] footstepAudioClipArray;
+
+    [SerializeField]
+    private AudioClip landingAudioClip;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
