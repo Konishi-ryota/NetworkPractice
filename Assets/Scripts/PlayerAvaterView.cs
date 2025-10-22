2680edcee097ac5a72b349f7dfe5c0bc06fb0325
ryotakonishi <ryota.konishi@icloud.com>
2025-10-22 21:24:46 +0900

    [add]変数追加

--- Assets/Scripts/PlayerAvaterView.cs
+++ Assets/Scripts/PlayerAvaterView.cs
@@ -7,6 +7,8 @@ public class PlayerAvaterView : MonoBehaviour
     [SerializeField]
     private CinemachineCamera cinemachineCamera;
 
+    [SerializeField]
+    private TextMeshPro nameLabel;
     public void MakeCameraTarget()
     {
         // CinemachineCamera‚Ì—Dæ“x‚ğã‚°‚ÄAƒJƒƒ‰‚Ì’Ç]‘ÎÛ‚É‚·‚é
